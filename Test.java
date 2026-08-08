bb83adf882e1178d0be43ac8836b3775dd2e98da
Atif-Md <125504481+Atif-Md@users.noreply.github.com>
2026-08-08 19:14:37 +0530

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.Calendar;
-
-public class Test {
-    public static void main(String[] args) throws Exception {
-		
-		System.out.println(Calendar.getInstance().get(Calendar.MONTH));
-    }
-}
