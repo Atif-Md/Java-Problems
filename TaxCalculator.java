ae1c0537e6b3f9fd94fe634b6d33046bcda7c02a
Atif <mdatif0422@gmail.com>
2026-08-09 16:12:19 +0530

    initial commit

---
+++ TaxCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your annual income: ");
+        double income = sc.nextDouble();
+
+        double tax = 0;
+
+        if(income < 500000)
+            tax = 0;
+        else if(income >= 500000 && income < 1000000)
+            tax = income * 0.2;
+        else
+            tax = income * 0.3;
+
+        System.out.println("Your tax is: "+tax);
+
+    }
+}
