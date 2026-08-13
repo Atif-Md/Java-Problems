0bd195b3d0737e6e0e3f37fc6ca7db23f056c0a6
Atif <mdatif0422@gmail.com>
2026-08-13 19:29:40 +0530

    added a loop and used a label, so we can check multiple numbers in single run

--- CheckPrime.java
+++ CheckPrime.java
@@ -3,19 +3,30 @@ import java.util.Scanner;
 public class CheckPrime {
     public static void main(String[] args) {
         //Check if a number is prime or not
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to check if it is prime or not:");
-        int num = sc.nextInt();
+        loop:
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number to check if it is prime or not:");
+            int num = sc.nextInt();
 
-        boolean isPrime = true;
-        for(int i = 2 ; i < num ; i++) {
-            if( num%i == 0)
-                isPrime = false;
+            boolean isPrime = true;
+            for(int i = 2 ; i <= Math.sqrt(num) ; i++) {
+                if( num%i == 0)
+                    isPrime = false;
+            }
+
+            if(isPrime)
+                System.out.println(num + " is a prime number");
+            else
+                System.out.println(num + " is not a prime number");
+
+            System.out.println("Do you want to check more number? Press 1 for yes and 0 for no: ");
+            int check = sc.nextInt();
+
+            if (check == 0) {
+                break loop;
+            }
         }
 
-        if(isPrime)
-            System.out.println(num + " is a prime number");
-        else
-            System.out.println(num + " is not a prime number");
     }
 }
