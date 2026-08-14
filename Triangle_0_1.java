43befbe2f9e79776dced639491c08cde8fa1acfc
Atif <mdatif0422@gmail.com>
2026-08-14 22:22:05 +0530

    improve the performance

--- Triangle_0_1.java
+++ Triangle_0_1.java
@@ -7,24 +7,42 @@ class Triangle_0_1
 		System.out.print("Enter number of rows : ");
 		int row = sc.nextInt();
 		
-		for (int i = 1; i<=row ; i++ )
-		{
-			int number=1;
-			if (i%2==0)
-			{
-				number=0;
-			}
-			for (int j=1 ; j<=i ; j++ )
-			{
-				System.out.print(number+" ");
-				if (number == 1)
-				{
-					number = 0;
-				}
-				else 
-					number = 1;
+		// for (int i = 1; i<=row ; i++ )
+		// {
+		// 	int number=1;
+		// 	if (i%2==0)
+		// 	{
+		// 		number=0;
+		// 	}
+		// 	for (int j=1 ; j<=i ; j++ )
+		// 	{
+		// 		System.out.print(number+" ");
+		// 		if (number == 1)
+		// 		{
+		// 			number = 0;
+		// 		}
+		// 		else 
+		// 			number = 1;
+		// 	}
+		// 	System.out.println();
+		// }
+
+
+
+		
+		for(int i = 1 ; i <= row ; i++) {
+			for(int j = 1 ; j <= i ; j++) {
+				if((i+j)%2 == 0)
+					System.out.print(1);
+				else
+					System.out.print(0);
 			}
 			System.out.println();
 		}
+
+
+
+
+
 	}
 }
