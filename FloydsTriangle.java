de9fad0d458412bd496496511f939929674e57a3
Atif <mdatif0422@gmail.com>
2026-08-14 22:04:11 +0530

    recall the logic again

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -6,16 +6,32 @@ class FloydsTriangle
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Number of rows : ");
 		int row = sc.nextInt();
+		
+		
+		// int k = 1;
+		// for (int i = 1; i<=row ; i++ )
+		// {
+		// 	for (int j=1;j<=i ;j++ )
+		// 	{
+		// 		System.out.print(k+" ");
+		// 		k++;
+		// 	}
+		// 	System.out.println();
+		// }
+
+
+
+
 		int k = 1;
-		for (int i = 1; i<=row ; i++ )
-		{
-			for (int j=1;j<=i ;j++ )
-			{
-				System.out.print(k+" ");
-				k++;
+		for(int i = 1 ; i <= row ; i++) {
+			for(int j = 1 ; j <= i ; j++) {
+				System.out.print(k++ + " ");
 			}
 			System.out.println();
 		}
+
+
+
 		
 	}
 }
