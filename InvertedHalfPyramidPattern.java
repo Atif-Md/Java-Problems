d46bd3999b531125875047c2ad4695664b4a66f2
Atif <mdatif0422@gmail.com>
2026-08-14 21:37:06 +0530

    recall it again

--- InvertedHalfPyramidPattern.java
+++ InvertedHalfPyramidPattern.java
@@ -3,33 +3,51 @@ class InvertedHalfPyramidPattern
 {
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter Number of Rows : ");
-		int row =sc.nextInt();
+		// Scanner sc = new Scanner(System.in);
+		// System.out.print("Enter Number of Rows : ");
+		// int row =sc.nextInt();
 		
 
-		//First Approach
+		// //First Approach
 
-		/*
-		for (int i=1;i<=row ;i++ )
-		{
-			for (int j=row;j>=i ;j-- )
-			{
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
+		// /*
+		// for (int i=1;i<=row ;i++ )
+		// {
+		// 	for (int j=row;j>=i ;j-- )
+		// 	{
+		// 		System.out.print("* ");
+		// 	}
+		// 	System.out.println();
+		// }
 
-		*/
+		// */
 
 
-		//Second Approach
+		// //Second Approach
 
-		for (int i=row;i>=1 ;i-- )
-		{
-			for (int j=1;j<=i ;j++ )
-			{
-				System.out.print("* ");
+		// for (int i=row;i>=1 ;i-- )
+		// {
+		// 	for (int j=1;j<=i ;j++ )
+		// 	{
+		// 		System.out.print("* ");
+		// 	}
+		// 	System.out.println();
+		// }
+
+
+
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number of rows: ");
+		int rows = sc.nextInt();
+
+		for(int i = 1 ; i <= rows ; i++) {
+			for(int j = 1 ; j <= rows ; j++) {
+				if(j <= rows - i)
+					System.out.print("  ");
+				else
+					System.out.print("* ");
 			}
 			System.out.println();
 		}
