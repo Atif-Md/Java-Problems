a386b35cd85dfbf00666e7f5c36691fb5f786921
Atif <mdatif0422@gmail.com>
2026-08-14 19:32:37 +0530

    recall it again

--- HollowRectangle.java
+++ HollowRectangle.java
@@ -3,29 +3,52 @@ class HollowRectangle
 {
 	public static void main(String[] args) 
 	{
+		// Scanner sc = new Scanner(System.in);
+
+		// System.out.print("Enter number of rows : ");
+		// int row = sc.nextInt();
+
+		// System.out.print("Enter number of column : ");
+		// int col = sc.nextInt();
+
+		// for (int i=1; i<=row ; i++ )
+		// {
+		// 	for (int j=1 ; j<=col ; j++ )
+		// 	{
+		// 		if (i == 1 || j == 1 || i == row || j == col)
+		// 		{
+		// 			System.out.print("* ");
+		// 		}
+		// 		else 
+		// 		{
+		// 			System.out.print("  ");
+		// 		}
+		// 	}
+		// 	System.out.println();
+
+
+
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter number of rows : ");
-		int row = sc.nextInt();
+		System.out.println("Enter number of rows: ");
+		int rows = sc.nextInt();
 
-		System.out.print("Enter number of column : ");
-		int col = sc.nextInt();
+		System.out.println("Enter number of columns: ");
+		int column = sc.nextInt();
 
-		for (int i=1; i<=row ; i++ )
-		{
-			for (int j=1 ; j<=col ; j++ )
-			{
-				if (i == 1 || j == 1 || i == row || j == col)
-				{
+		for(int i = 1 ; i <= rows ; i++) {
+			for(int j = 1 ; j <= column ; j++) {
+				if(i == 1 || i == rows || j == 1 || j == column)
 					System.out.print("* ");
-				}
-				else 
-				{
+				else
 					System.out.print("  ");
-				}
+
 			}
 			System.out.println();
 		}
+
+
+
 	}
 }
 
