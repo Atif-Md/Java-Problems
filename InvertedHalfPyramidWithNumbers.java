4a466c4d21bbe85e6cd915595fcf044c46e619da
Atif <mdatif0422@gmail.com>
2026-08-14 21:51:33 +0530

    recall the logic

--- InvertedHalfPyramidWithNumbers.java
+++ InvertedHalfPyramidWithNumbers.java
@@ -6,14 +6,30 @@ class InvertedHalfPyramidWithNumbers
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Number of rows : ");
 		int row = sc.nextInt();
-		for (int i = row ; i>=1 ; i-- )
-		{
-			for (int j = 1 ; j<=i ; j++ )
-			{
-				System.out.print(j+" ");
+		
+		
+		// for (int i = row ; i>=1 ; i-- )
+		// {
+		// 	for (int j = 1 ; j<=i ; j++ )
+		// 	{
+		// 		System.out.print(j+" ");
+		// 	}
+		// 	System.out.println();
+		// }
+		
+
+
+
+		for(int i = 1 ; i <= row ; i++) {
+			for(int j = 1 ; j <= row - i + 1 ; j++) {
+				System.out.print(j);
 			}
 			System.out.println();
 		}
-		
+
+
+
+
+
 	}
 }
